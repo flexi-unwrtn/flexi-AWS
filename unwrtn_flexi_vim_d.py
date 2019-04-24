2ffd6263ff79affb32dbf714acb107297da7222a
Stephen Kelly <stephen@unwr.tn>
2019-04-24 13:19:09 +0800

    Update unwrtn_flexi_vim_d.py

--- unwrtn_flexi_vim_d.py
+++ unwrtn_flexi_vim_d.py
@@ -13,7 +13,7 @@ for row in reader:
         headers = row
     else:
         crow = dict(zip(headers, row))
-        html = html + ':edit '+crow['Const']+'.html\n:%s+href="/+href="https://www.imdb.com/+g\n:/egin TOP_AD/,/nd TOP_AD/d\n:/Episode aired/\n:read ' +crow['Const']+'.c\n:write\n'
+        html = html + ':edit '+crow['Const']+'.html\n:%s+href="/+href="https://www.imdb.com/+g\n:/All Episodes/-4s+href="https://www.imdb.com/+href="/+\n:s+/episodes+.html+\n:?Previous?-4s+href="https://www.imdb.com/+href="/+\n:/Next/-4s+href="https://www.imdb.com/+href="/+\n:/egin TOP_AD/,/nd TOP_AD/d\n:/Episode aired/\n:read ' +crow['Const']+'.c\n:write\n'
         f = open('flexi.html','w')
 f.write(html)
 f.close()
