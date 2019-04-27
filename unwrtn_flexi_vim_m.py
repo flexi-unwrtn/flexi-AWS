dd29ea5b0a43c23cb5f937daa4894c3f476fdcd7
Stephen Kelly <stephen@unwr.tn>
2019-04-27 15:20:17 +0800

    Update unwrtn_flexi_vim_m.py

--- unwrtn_flexi_vim_m.py
+++ unwrtn_flexi_vim_m.py
@@ -13,7 +13,7 @@ for row in reader:
         headers = row
     else:
         crow = dict(zip(headers, row))
-        html = html + ':edit '+crow['Const']+'.html\n:%s+href="/+href="https://m.imdb.com/+g\n:/arrow-right/-3s+href="https://m.imdb.com/+href="/+\n:s+/episodes+.html+\n:/arrow-right/-1s+href="https://m.imdb.com/+href="/+\n:s+/"+.html"+\n:/arrow-left/-1s+href="https://m.imdb.com/+href="/+\n:s+/"+.html"+\n:/egin TOP_AD/,/nd TOP_AD/d\n:/Season/\n:read ' +crow['Const']+'.c\n:write\n'
+        html = html + ':edit '+crow['Const']+'.html\n:%s+href="/+href="https://m.imdb.com/+g\n:/arrow-right/-3s+href="https://m.imdb.com/+href="/+\n:s+/episodes+.html+\n:/arrow-right/-1s+href="https://m.imdb.com/+href="/+\n:s+/"+.html"+\n:/arrow-left/-1s+href="https://m.imdb.com/+href="/+\n:s+/"+.html"+\n:/egin TOP_AD/,/nd TOP_AD/d\n:/Season</\n:read ' +crow['Const']+'.c\n:write\n'
         f = open('flexi.html','w')
 f.write(html)
 f.close()
